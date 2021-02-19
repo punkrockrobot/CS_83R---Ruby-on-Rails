b4fb1a822cf97ffd2ca526ae9c28deefaec27074
Brian Worth <brianworth007@gmail.com>
2021-02-18 19:19:37 -0800

    Table

---
+++ xTable.rb
@@ -0,0 +1,27 @@
+=begin
+Brian Worth
+1830
+10/2/2020
+create a 5x5 X table
+=end
+
+
+print "Gimme a starting value: "
+numb = gets()
+
+pony = [numb.to_i, numb.to_i+1, numb.to_i+2, numb.to_i+3, numb.to_i]
+a,b,c,d,a = pony
+tea = [numb.to_i+4, numb.to_i, numb.to_i+5, numb.to_i, numb.to_i+6]
+e,a,f,a,g = tea
+jelly = [numb.to_i+7, numb.to_i+8, numb.to_i, numb.to_i+9, numb.to_i+10]
+h,i,a,j,k = jelly
+button = [numb.to_i+11, numb.to_i, numb.to_i+12, numb.to_i, numb.to_i+13]
+l,a,m,a,n = button
+goats = [numb.to_i, numb.to_i+14, numb.to_i+15, numb.to_i+16, numb.to_i]
+a,o,z,q,a = goats
+
+print a," ",b," ",c," ",d," ",a, "\n"
+print e," ",a," ",f," ",a," ",g, "\n"
+print h," ",i," ",a," ",j," ",k, "\n"
+print l," ",a," ",m," ",a," ",n, "\n"
+print a," ",o," ",z," ",q," ",a, "\n"
