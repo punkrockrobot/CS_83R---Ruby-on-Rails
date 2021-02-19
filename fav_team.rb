6d0fc872787e623b3975f63596ee02d6da98ac75
Brian Worth <brianworth007@gmail.com>
2021-02-18 19:00:36 -0800

    Add files via upload

---
+++ fav_team.rb
@@ -0,0 +1,12 @@
+=begin
+Brian Worth
+1830
+10/2/2020
+Display Favorite team 
+=end
+
+puts "PPPPPP  A     NN   NN TTTTTT HH   HH  EEEEEE RRRRRR   SSSSS  !"
+puts "PP  PP A A    NNN  NN   TT   HH   HH  EEE    RR  RR   S      !"
+puts "PPPPP AAAAA   NN N NN   TT   HHHHHHH  EEEEEE RRRRRR   SSSSS  !"
+puts "PP   AA   AA  NN  NNN   TT   HH   HH  EEE    RR   RR      S   "
+puts "PP  AA     AA NN   NN   TT   HH   HH  EEEEEE RR    RR SSSSS  !"
